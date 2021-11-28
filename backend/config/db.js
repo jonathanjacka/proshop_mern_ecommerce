42ea7ffcd14138f3288733b148d5d0e54b2105d8
Jonathan Jacka <jonathanjacka@gmail.com>
2021-11-28 18:45:47 +0200

    Updated models

---
+++ backend/config/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+import colors from 'colors';
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+
+        console.log(`MongoDB connected: ${conn.connection.host}`.bgGreen.black);
+    } catch (error) {
+        console.error(`Error: ${error.message}`.bgRed);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import colors from 'colors';
+
 import products from './data/products.js';
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
@@ -24,6 +28,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(
-    `Server running on in ${process.env.NODE_ENV} port ${PORT}: Hello there...`
+    `Server running on in ${process.env.NODE_ENV} port ${PORT}: Hello there...`.bgBlue
   )
 );
