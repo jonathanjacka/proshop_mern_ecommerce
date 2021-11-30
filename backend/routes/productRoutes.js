ee31b714a3f5a50ae6b71c00ff64503453634ad2
Jonathan Jacka <jonathanjacka@gmail.com>
2021-11-30 08:09:14 +0200

    Updated backend routes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+const router = express.Router();
+import Product from '../models/ProductModel.js';
+
+// @desc    Fetch all products
+// @routes  GET /api/products
+// @access  Public
+router.get('/', asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+
+    res.json(products);
+  }));
+
+// @desc    Fetch single product by id
+// @routes  GET /api/products/:id
+// @access  Public
+router.get('/:id', asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+
+    if(!product) {
+        res.status(404).json({success: false, message: `Product with id of ${req.params.id} not found.  Either the product does not exist or the id you've entered is incorrect.`});
+    } else {
+        res.json(product);
+    }
+    
+}));
+
+export default router;
