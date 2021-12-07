4905342778fb1a85778344dff65271eed35970f7
Jonathan Jacka <jonathanjacka@gmail.com>
2021-12-06 19:53:02 -0600

    Updated homeScreen to access productList state via redux

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,25 +6,31 @@ import Product from '../models/ProductModel.js';
 // @desc    Fetch all products
 // @routes  GET /api/products
 // @access  Public
-router.get('/', asyncHandler(async (req, res) => {
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
     const products = await Product.find({});
-
     res.json(products);
-  }));
+  })
+);
 
 // @desc    Fetch single product by id
 // @routes  GET /api/products/:id
 // @access  Public
-router.get('/:id', asyncHandler(async (req, res) => {
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
 
-    if(!product) {
-        res.status(404);
-        throw new Error(`Product with id of ${req.params.id} not found.  Either the product does not exist or the id you've entered is incorrect.`);
+    if (!product) {
+      res.status(404);
+      throw new Error(
+        `Product with id of ${req.params.id} not found.  Either the product does not exist or the id you've entered is incorrect.`
+      );
     } else {
-        res.json(product);
+      res.json(product);
     }
-    
-}));
+  })
+);
 
 export default router;
