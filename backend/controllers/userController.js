4cbff779ae591ffde7674b1b6fa48ad95ade7cad
Jonathan Jacka <jonathanjacka@gmail.com>
2021-12-12 19:45:52 -0600

    Added create user functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -23,6 +23,38 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Register a new user
+// @routes  POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User with chosen email address already exists.');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (!user) {
+    res.status(400);
+    throw new Error('Invalid user data - unable to create profile');
+  } else {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  }
+});
+
 // @desc    Get user profile
 // @routes  GET /api/users/profile
 // @access  Private
@@ -42,4 +74,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile };
+export { authUser, getUserProfile, registerUser };
