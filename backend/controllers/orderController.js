d40153c9b8e6318b735c21f929b4743fe45f07b6
Jonathan Jacka <jonathanjacka@gmail.com>
2021-12-21 15:21:54 -0600

    Added get order by id control and route

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -35,4 +35,23 @@ const addOrderItems = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems };
+// @desc    Get order by ID
+// @routes  GET /api/orders/:id
+// @access  Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate(
+    'user',
+    'name email'
+  );
+
+  if (!order) {
+    res.status(404);
+    throw new Error(
+      'Error not found - either the order does not exist or the order id is incorrect.'
+    );
+  } else {
+    res.json(order);
+  }
+});
+
+export { addOrderItems, getOrderById };
