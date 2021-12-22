0a64c7c0b31bcdb96d7487aa4753dd1e7fafe969
Jonathan Jacka <jonathanjacka@gmail.com>
2021-12-22 17:05:29 -0600

    Added PayPal payment on Order screen - error with totalPrice format

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -8,6 +8,8 @@ import {
   ORDER_DETAILS_REQUEST,
   ORDER_DETAILS_SUCCESS,
   ORDER_DETAILS_FAIL,
+  ORDER_PAY_REQUEST,
+  ORDER_PAY_SUCCESS,
 } from '../constants/orderConstants';
 
 export const createOrder = (order) => async (dispatch, getState) => {
@@ -82,3 +84,45 @@ export const getOrderDetails = (id) => async (dispatch, getState) => {
       : toast.error(`${error.message}`, { autoClose: 5000 });
   }
 };
+
+export const payOrder =
+  (orderId, paymentResult) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: ORDER_PAY_REQUEST,
+      });
+
+      const {
+        userLogin: { userInfo },
+      } = getState();
+
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+
+      const { data } = await axios.put(
+        `/api/orders/${orderId}/pay`,
+        paymentResult,
+        config
+      );
+
+      dispatch({
+        type: ORDER_PAY_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: ORDER_DETAILS_FAIL,
+        payload:
+          error.response && error.response.data.message
+            ? error.response.data.message
+            : error.message,
+      });
+      error.response && error.response.data.message
+        ? toast.error(`${error.response.data.message}`, { autoClose: 5000 })
+        : toast.error(`${error.message}`, { autoClose: 5000 });
+    }
+  };
