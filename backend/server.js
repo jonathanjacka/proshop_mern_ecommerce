7c06df6ac1e9caa6c6b52f4408576e7f7d024258
Jonathan Jacka <jonathanjacka@gmail.com>
2022-01-01 08:15:05 -0600

    Updated with frontend production build

--- backend/server.js
+++ backend/server.js
@@ -42,10 +42,6 @@ app.use(limiter);
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is up and running...');
-});
-
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
@@ -64,6 +60,17 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 //Error Handlers
 app.use(notFound);
